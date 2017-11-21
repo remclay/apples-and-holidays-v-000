53696b52df87ee972f9afa4c2b7044c1950ad5b0
Remmy Clay <remmy.lauren@gmail.com>
2017-11-21 01:29:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,36 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter].each do |holiday, decorations|
+    decorations << supply
+  end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  # holiday_hash[:spring][:memorial_day] << supply
+holiday_hash[:spring][:memorial_day] = supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+  holiday_hash[season][holiday_name] = supply_array
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,18 +58,53 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do |season, holiday|
+    season_string = season.to_s
+    puts "#{season_string.capitalize}:"
+    holiday.each do |occasion, decoration|
+      occ = occasion.to_s.split('_')
+      occ.each {|word| word.capitalize!}
+    puts "  #{occ.join(" ")}: #{decoration.join(", ")}"
+    end
+  end
 end
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.each do |season, holiday|
+     holiday.map do |occasion, decorations|
+       occasion if decorations.include?("BBQ")
+     end
+end
 end
 
 
+#  holidays_with_bbqs = []
+#  holiday_hash.each do |season, holiday|
+#    holiday.each do |occasion, decorations|
+#      if decorations.include?("BBQ")
+#      holidays_with_bbqs <<  occasion
+#    end
+#  end
+#end
+#holidays_with_bbqs
+#end
 
 
-
-
-
+all_holidays_with_bbq({
+  :winter => {
+    :christmas => ["Lights", "Wreath"],
+    :new_years => ["Party Hats"]
+  },
+  :summer => {
+    :fourth_of_july => ["Fireworks", "BBQ"]
+  },
+  :fall => {
+    :thanksgiving => ["Turkey"]
+  },
+  :spring => {
+    :memorial_day => ["BBQ"]
+  }
+})
